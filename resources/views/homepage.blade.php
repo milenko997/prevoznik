7568372efd45fa5dc4827165b20f720b20d62dde
Milenko Ilic <milenko.ilic997@gmail.com>
2025-05-13 11:35:04 +0200

    Updating page view

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,34 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Homepage') }}
+        </h2>
+    </x-slot>
 
-        <title>{{ config('app.name', 'Prevoznik') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
-
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    All Advertisements
+                </div>
             </div>
-        </header>
-
-        <!-- Page Content -->
-        <main>
-            {{--                {{ $slot }}--}}
-        </main>
+        </div>
     </div>
-    </body>
-</html>
+</x-app-layout>
