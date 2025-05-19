ae600ef2f8872807da885e3bf0ea8742ac5c5d07
Milenko Ilic <milenko.ilic997@gmail.com>
2025-05-19 14:49:10 +0200

    Creating single ad page

---
+++ resources/views/advertisements/show.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-capitalize">
+            {{ $ad->title }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white">
+                    @if($ad->image)
+                        <img src="{{ asset('storage/' . $ad->image) }}" alt="image" class="w-50 h-auto mb-4">
+                    @endif
+
+                    <p class="text-gray-700 mb-2">{{ $ad->description }}</p>
+
+                    <p class="text-sm text-gray-500 mt-2">Posted: {{ $ad->created_at->format('d.m.Y') }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
